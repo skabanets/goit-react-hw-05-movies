da3444745913c8c3753229ee8afa3cd9761bbd73
Sergey Kabanets <skabanets97@gmail.com>
2024-01-18 18:07:38 +0200

    add basic global styles and container

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import { App } from 'components/App';
 import './index.css';
 import 'modern-normalize/modern-normalize.css';
 import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from 'styles/CreateGlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/goit-react-hw-05-movies">
-    <App />
-  </BrowserRouter>
+  <>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
+      <App />
+      <GlobalStyle />
+    </BrowserRouter>
+  </>
 );
